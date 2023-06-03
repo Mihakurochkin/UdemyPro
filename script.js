4983ce205f3a46c749caf1dd6944a80c2be3aac8
Mykhailo <kurochkinmy@gmail.com>
2023-06-03 12:34:00 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,72 @@
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
+
+    while(!numberOfFilms) {
+        numberOfFilms = +prompt('Скільки фільмів ви переглянули?', '');
+    }
+
+}
+
+start();
+
+const personalMovieDb = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+
+
+function rememberMyFilms() {
+    for(let i = 0; i < 2; i++){
+        const a = prompt('Назвіть один з переглянутих фільмів', ''),
+            b = prompt('На скільки його оціните?', '');
+
+        if(a && b && a.length < 50) {
+            personalMovieDb.movies[a] = b;
+            console.log('done')
+        } else {
+            console.log('error')
+            i--;
+        }
+
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if(personalMovieDb.count < 10) {
+        alert('Переглянуто доволі мало фільмів');
+    } else if(10 <= personalMovieDb.count <= 30 ) {
+        alert('Ви класичний глядач!');
+    } else if(personalMovieDb.count > 30) {
+        alert('Ви кіноман!');
+    } else {
+        alert('Сталася помилка!');
+    }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+    if(!hidden) {
+        console.log(personalMovieDb);
+    }
+}
+
+showMyDB(personalMovieDb.privat);
+
+function writeMyGenres() {
+    for(let i = 1; i <= 3; i++) {
+        personalMovieDb.genres[i -1] = prompt(`Ваш улюблений жанр під номером ${i}`, '');
+    }
+}
+
+writeMyGenres();
+
+console.log(personalMovieDb.genres);
