6064c927d56691b33c4f7ff8eb8e2b658d8a5a93
Mykhailo Kurochkin <kurochkinmy@gmail.com>
2023-08-04 17:24:36 +0300

    added compendium lesson40.js

---
+++ compendium/lesson40.js
@@ -0,0 +1,15 @@
+function createCounter() {
+  let counter = 0;
+
+  return function () {
+    return ++counter
+  };
+}
+
+const increment = createCounter();
+const c1 = increment();
+const c2 = increment();
+const c3 = increment();
+
+
+console.log(c1, c2, c3);
