8e3bdf95d22df9a7112f584f3ae086a90173e132
Mykhailo Kurochkin <kurochkinmy@gmail.com>
2023-08-09 15:08:48 +0300

    task #16 done

---
+++ tasks/task16.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+  if (typeof (n) !== 'number' || !Number.isInteger(n)) {
+    return 'Помилка, перевірте дані';
+  }
+
+  if (n >= 1) {
+    return n * factorial(n - 1);
+  } else {
+    return 1;
+  }
+}
