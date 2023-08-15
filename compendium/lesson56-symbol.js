319cd582c4a1acfd00b17d6fef50e21fcc4cd650
Mykhailo Kurochkin <kurochkinmy@gmail.com>
2023-08-15 11:18:08 +0300

    lesson56-symbol.js completed

---
+++ compendium/lesson56-symbol.js
@@ -0,0 +1,30 @@
+let id = Symbol('id');
+
+const obj = {
+  'name': 'Test',
+  [Symbol('id')] : 1,
+  getId: function() {
+    return this[id];
+  }
+}
+
+// let id = Symbol('id');
+//
+// obj[id] = 1;
+
+console.log(obj[Object.getOwnPropertySymbols(obj)[0]]);
+
+for(let value in obj) console.log('', value);
+
+const myAwesomeDB = {
+  movies: [],
+  actors: [],
+  [Symbol('id')]: 123
+}
+
+//
+
+myAwesomeDB.id = '24342987'
+
+console.log('', myAwesomeDB['id']);
+console.log('', myAwesomeDB);
